788b0ac125244bee6da1f5628d6a536d08e746b1
Aatu Hirvonen <aatu95@gmail.com>
2019-08-12 15:38:32 +0300

    shopping cart total price

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -5,9 +5,12 @@ import Product from './Product'
 import { emptyShoppingCart } from '../reducers/shoppingCartReducer'
 
 const ShoppingCart = (props) => {
-  if (!props) {
+  const totalPrice = cart => cart.reduce((prev, curr) => prev + curr.details.price, 0)
+
+  if (!props.cart) {
     return <h2>Shopping cart</h2>
   }
+  console.log('cart', props.cart)
   return (
     <div>
       <h2>Shopping Cart</h2>
@@ -18,6 +21,12 @@ const ShoppingCart = (props) => {
           </div>
         ))}
       </ul>
+      <p>
+Total price:
+        {totalPrice(props.cart)}
+        {' '}
+€
+      </p>
       <button type="button" onClick={() => props.emptyShoppingCart()}>
         clear cart
       </button>
